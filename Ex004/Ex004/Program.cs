f7434639a8fd9b913d3779cb8ab2a486470722aa
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-14 13:28:40 -0300

    Atividades e Desafios

--- Ex004/Ex004/Program.cs
+++ Ex004/Ex004/Program.cs
@@ -1,11 +1,14 @@
-﻿Album albumDoQueen = new Album();
+﻿Banda queen = new Banda();
+queen.Nome = "Queen";
+
+Album albumDoQueen = new Album();
 albumDoQueen.Nome = "A night at the opera";
 
-musica musica1 = new musica();
+musica musica1 = new musica(queen);
 musica1.Nome = "Love of my life";
 musica1.Duracao = 213;
 
-musica musica2 = new musica();
+musica musica2 = new musica(queen);
 musica2.Nome = "Bohemian Rhapsody";
 musica2.Duracao = 354;
 
@@ -13,3 +16,6 @@ albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
 
 albumDoQueen.ExibirMusicasDoAlbum();
+
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
