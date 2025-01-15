a8fa5a1da8d052181ac3456ee9db02434d04b95d
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-15 12:53:49 -0300

    Desafios e Ex

---
+++ Desafio021/Desafio021/Program.cs
@@ -0,0 +1,25 @@
+﻿//Criar um programa Program.cs, instanciar seus 5 filmes favoritos,
+//guardá-los em uma lista e mostrar as suas informações no console.
+
+using Alura.Filmes;
+
+Filme filme = new Filme("Um sonho de liberdade", 142, new List<string>() { "Tim Robbins", "Morgan Freeman" });
+Filme filme2 = new Filme("O poderoso chefão", 175, new List<string>() { "Marlon Brando", "Al Pacino", "Talia Shire" });
+Filme filme3 = new Filme("Batman - O Cavaleiro das Trevas", 152, new List<string>() { "Christian Bale", "Heath Ledger", "Maggie Gyleenhaal" });
+Filme filme4 = new Filme("Senhor dos Anéis - O Retorno do Rei", 201, new List<string>() { "Elijah Wood", "Ian McKellen", "Viggo Mortensen" });
+Filme filme5 = new Filme("Green Book - O Guia", 130, new List<String>() { "Viggo Mortensen", "Mahershala Ali" });
+
+List<Filme> meusFilmesFavoritos = new List<Filme>();
+meusFilmesFavoritos.Add(filme);
+meusFilmesFavoritos.Add(filme2);
+meusFilmesFavoritos.Add(filme3);
+meusFilmesFavoritos.Add(filme4);
+meusFilmesFavoritos.Add(filme5);
+
+foreach (Filme f in meusFilmesFavoritos)
+{
+    Console.WriteLine($"Filme: {f.Titulo}");
+    Console.WriteLine($"Duracao: {f.Duracao}");
+    f.ListarElenco();
+    Console.WriteLine();
+}
---
+++ Desafio022/Desafio022/Program.cs
@@ -0,0 +1,44 @@
+﻿//Criar uma classe Artista, que representa uma pessoa que atua em filmes, no namespace Alura.Filmes.
+//A classe deve conter atributos como o nome, idade e uma lista de filmes onde o artista atuou.
+
+namespace Alura.Filmes
+{
+
+    public class Artista
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public List<Filme> FilmesAtuados { get; set; }
+        public int QuantidadeDeFilmes => FilmesAtuados.Count
+
+
+        public Artista(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            FilmesAtuados = new List<Filme>();
+        }
+
+
+        public void AdicionarFilme(string filme)
+        {
+            FilmesAtuados.Add(filme);
+        }
+
+        public void MostrarFilmesAtuados()
+        {
+
+            if (this.FilmesAtuados.Count == 0)
+            {
+                Console.WriteLine($"Nenhum filme encontrado na base para {this.Nome}");
+                return;
+            }
+
+            Console.WriteLine($"Filmes de {this.Nome}...");
+            foreach (var filme in FilmesAtuados)
+            {
+                Console.WriteLine($"Filme: {filme.Titulo}");
+            }
+        }
+    }
+}
