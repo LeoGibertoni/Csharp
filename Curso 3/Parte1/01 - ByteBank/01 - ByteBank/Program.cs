f158768cdd2bada6bb4ace09dd314d4f8318f894
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-31 10:02:29 -0300

    att

---
+++ Curso 3/Parte1/01 - ByteBank/01 - ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Gabriela";
+            int numeroAgencia = 863;
+            int numero = 83532;
+            double saldo = 150;
+
+            string titular2 = "Bruno";
+            int numeroAgencia2 = 863;
+            int numero2 = 6233;
+            double saldo2 = 150;
+        }
+    }
+}
