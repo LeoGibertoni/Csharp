3af62c1730af6f33fb39827739ff6fe3b08a821c
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-03-10 18:18:31 -0300

    att

---
+++ Crud/AwesomeDevEvents/AwesomeDevEvents.API/Migrations/20250221142221_primeiro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AwesomeDevEvents.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class primeiro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "devEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    Start_Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "End_Date", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_devEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DevEventSpeakers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TalkTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TalkDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkedInProfile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DevEventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevEventSpeakers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DevEventSpeakers_devEvents_Id",
+                        column: x => x.Id,
+                        principalTable: "devEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevEventSpeakers");
+
+            migrationBuilder.DropTable(
+                name: "devEvents");
+        }
+    }
+}
